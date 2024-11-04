3075c7e8e51c8726177655b0fc4b6d4f2c856051
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-11-04 13:22:54 +0430

    feat: freeze each die

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,35 @@ export default function App() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
       const randomNumber = {
-          value:  Math.ceil(Math.random() * 6), 
-          isHeld: false, 
-          id: nanoid()
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
       }
-     newDice.push(randomNumber)
+      newDice.push(randomNumber)
     }
 
-    return newDice 
+    return newDice
   }
 
-  function rollDice(){
-    setNumbers(allNewDice)
+  function rollDice() {
+    setNumbers(prev => prev.map(die => {
+      return die.isHeld ? die : {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }
+    }))
   }
 
-  function hold(id){
+  function hold(id) {
 
-      setNumbers(prev => prev.map(dice =>{
-        if(dice.id === id){
-          return {...dice, isHeld: !dice.isHeld}
-        }else{
-          return dice
-        }
-      }))
-
-    
-
-        // numbers.map(dice =>{
-        //   if(dice.id === id){
-        //       return 
-        //   }
-        // })
+    setNumbers(prev => prev.map(dice => {
+      if (dice.id === id) {
+        return { ...dice, isHeld: !dice.isHeld }
+      } else {
+        return dice
+      }
+    }))
   }
 
   const [numbers, setNumbers] = useState(allNewDice())
@@ -46,12 +44,14 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dices">
-      {
-          numbers.map((die, i) =>(
-            <Die key={die.id} isHeld={die.isHeld} onclick={()=> hold(die.id)} die={die.value} />
+        {
+          numbers.map((die, i) => (
+            <Die key={die.id} isHeld={die.isHeld} onclick={() => hold(die.id)} die={die.value} />
           ))
-      }
+        }
       </div>
       <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
