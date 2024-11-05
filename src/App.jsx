ef9bf07ad073168c0f7c90a85c36146be25bed0b
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-11-05 12:25:04 +0430

    feat: Enhance Tenzies game with new features and improved user experience
    - Added functionality to end the game and display a win message
    - Updated dice representation from numbers to dots for a more authentic look
    - Implemented a timer to track the duration of each game session
    - Introduced a roll count feature to keep track of the number of rolls made
    - Included celebratory confetti animation upon winning the game for visual feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti';
 
 export default function App() {
+
+
+  const [numbers, setNumbers] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false);
+  const [rollCounts, setRollCounts] = useState(0)
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+
+
+
+
+
+  useEffect(() => {
+    const held = numbers.every(dice => {
+      return dice.isHeld
+    })
+    const firstValue = numbers[0].value
+    const isEqual = numbers.every(dice => dice.value === firstValue)
+    if (held && isEqual) {
+      setTenzies(true)
+      setIsRunning(false);
+
+    }
+
+  }, [numbers])
+
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000); // Update every second
+    } else if (!isRunning && time !== 0) {
+      clearInterval(interval);
+    }
+
+    // Cleanup interval on component unmount or when isRunning changes
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+
 
   function allNewDice() {
     const newDice = []
@@ -19,6 +64,8 @@ export default function App() {
   }
 
   function rollDice() {
+    setRollCounts(prev => prev + 1)
+    setIsRunning(true);
     setNumbers(prev => prev.map(die => {
       return die.isHeld ? die : {
         value: Math.ceil(Math.random() * 6),
@@ -29,7 +76,6 @@ export default function App() {
   }
 
   function hold(id) {
-
     setNumbers(prev => prev.map(dice => {
       if (dice.id === id) {
         return { ...dice, isHeld: !dice.isHeld }
@@ -39,21 +85,35 @@ export default function App() {
     }))
   }
 
-  const [numbers, setNumbers] = useState(allNewDice())
+  function reset() {
+    setNumbers(allNewDice())
+    setTenzies(false)
+    setRollCounts(0)
+    setIsRunning(true);
+    setTime(0);
+
+  }
 
 
   return (
     <main>
+
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dices">
+        {tenzies && <Confetti />}
         {
-          numbers.map((die, i) => (
+          numbers.map((die) => (
             <Die key={die.id} isHeld={die.isHeld} onclick={() => hold(die.id)} die={die.value} />
           ))
         }
       </div>
-      <button className='roll-dice' onClick={rollDice}>Roll</button>
+
+      <div className='roll-container' >
+        <p> <b> {rollCounts}</b> Times Rolled </p>
+        <button className='roll-dice' onClick={tenzies ? reset : rollDice}> {tenzies ? "New Game" : "Roll"}</button>
+        <p>Timer <br /> {time} s</p> 
+      </div>
     </main>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function Die(props) {
+
+  const divs = []
+
+  for (let i = 0; i < props.die; i++) {
+    divs.push(<div key={i}></div> )
+  }
+
   return (
-    <button  className={`dice ${props.isHeld ? "held": ''}`}  onClick={props.onclick}>{props.die}</button>
+    <button data-dot={props.die} className={`dice ${props.isHeld ? "held" : ''}`} onClick={props.onclick}>{ divs}</button>
   )
 }
